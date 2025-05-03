eb20429a81840b985eb76fb8a148afbca78b4ff1
EngElmenoufy <engelmenoufy@gmail.com>
2025-05-03 10:47:37 +0300

    Implemented notification component

--- src/layout/Header/Navbar/DropdownNotification.jsx
+++ src/layout/Header/Navbar/DropdownNotification.jsx
@@ -1,6 +1,21 @@
+import Notification from "./Notification";
+
 function DropdownYourAccount() {
   return (
-    <div className="absolute right-0 top-12 w-64 bg-white rounded-md shadow-lg border border-gray-200 z-50"></div>
+    <div className="absolute right-0 top-12 w-80 bg-white rounded-md shadow-lg border border-gray-200 z-50 pt-2">
+      <h5 className="text-center text-[#184033] mb-4">Notifications</h5>
+
+      <div className="h-96 overflow-y-auto">
+        <Notification />
+        <Notification />
+        <Notification />
+        <Notification />
+        <Notification />
+        <Notification />
+        <Notification />
+        <Notification />
+      </div>
+    </div>
   );
 }
 
---
+++ src/layout/Header/Navbar/Notification.jsx
@@ -0,0 +1,26 @@
+function Notification() {
+  return (
+    <div className="flex gap-3 border-b border[#E1E2DE]  p-2">
+      <img src="profile-image.avif" alt="profile" className="w-10 h-10" />
+      <div className="flex gap-3">
+        <div>
+          <p className="text-sm leading-4 text-[#0E150E] mb-2">
+            50% OFF of everything at Elegance
+          </p>
+          <p className="text-[#9FAAA6] text-xs font-medium">15 Oct</p>
+        </div>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          height="24px"
+          viewBox="0 -960 960 960"
+          width="24px"
+          fill="#1f1f1f"
+        >
+          <path d="M504-480 320-664l56-56 240 240-240 240-56-56 184-184Z" />
+        </svg>
+      </div>
+    </div>
+  );
+}
+
+export default Notification;
